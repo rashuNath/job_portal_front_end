4a90530bb1a7944a8018ed04cfaa24e1739734e1
rashuNath <rashu.web@gmai.com>
2019-04-08 16:18:27 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,292 @@
+/** 
+  * Template Name: Varsity
+  * Version: 1.0  
+  * Template Scripts
+  * Author: MarkUps
+  * Author URI: http://www.markups.io/
+
+  Custom JS
+  
+
+  1. SEARCH FORM
+  2. ABOUT US VIDEO
+  2. TOP SLIDER
+  3. ABOUT US (SLICK SLIDER) 
+  4. LATEST COURSE SLIDER (SLICK SLIDER) 
+  5. TESTIMONIAL SLIDER (SLICK SLIDER)
+  6. COUNTER
+  7. RELATED ITEM SLIDER (SLICK SLIDER)
+  8. MIXIT FILTER (FOR GALLERY)
+  9. FANCYBOX (FOR PORTFOLIO POPUP VIEW)  
+  11. HOVER DROPDOWN MENU
+  12. SCROLL TOP BUTTON  
+
+  
+**/
+
+jQuery(function($){
+
+
+  /* ----------------------------------------------------------- */
+  /*  1. SEARCH FORM
+  /* ----------------------------------------------------------- */
+
+   // jQuery('#mu-search-icon').on('click', function(event) {
+   //      event.preventDefault();
+   //      $('#mu-search').addClass('mu-search-open');
+   //      $('#mu-search form input[type="search"]').focus();
+   //  });
+    
+    jQuery('.mu-search-close').on('click', function(event) {
+      $("#mu-search").removeClass('mu-search-open');
+    });
+
+/* ----------------------------------------------------------- */
+/*  2. ABOUT US VIDEO
+/* ----------------------------------------------------------- */
+    // WHEN CLICK PLAY BUTTON 
+    jQuery('#mu-abtus-video').on('click', function(event) {
+      event.preventDefault();
+      $('body').append("<div id='about-video-popup'><span id='mu-video-close' class='fa fa-close'></span><iframe id='mutube-video' name='mutube-video' frameborder='0' allowfullscreen></iframe></div>");        
+      $("#mutube-video").attr("src", $(this).attr("href"));
+    });         
+    // WHEN CLICK CLOSE BUTTON
+    $(document).on('click','#mu-video-close', function(event) {     
+      $(this).parent("div").fadeOut(1000);
+    });
+    // WHEN CLICK OVERLAY BACKGROUND
+    $(document).on('click','#about-video-popup', function(event) {
+      $(this).remove();
+    });
+  
+  /* ----------------------------------------------------------- */
+  /*  3. TOP SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */    
+
+    jQuery('#mu-slider').slick({
+      dots: false,
+      infinite: true,
+      arrows: true,
+      speed: 500,     
+      autoplay: false,
+      cssEase: 'linear'
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  4. ABOUT US (SLICK SLIDER)
+  /* ----------------------------------------------------------- */      
+
+    jQuery('#mu-testimonial-slide').slick({
+      dots: true,
+      infinite: true,
+      arrows: false,
+      speed: 500,
+      autoplay: true,          
+      cssEase: 'linear'
+    });
+  
+
+  /* ----------------------------------------------------------- */
+  /*  5. LATEST COURSE SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */    
+
+    jQuery('#mu-latest-course-slide').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll:1,
+      autoplay: false,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  6. TESTIMONIAL SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */    
+
+    jQuery('.mu-testimonial-slider').slick({
+      dots: true,      
+      infinite: true,
+      arrows: false,
+      autoplay: true,
+      speed: 500,      
+      cssEase: 'linear'
+    });       
+
+  /* ----------------------------------------------------------- */
+  /*  7. COUNTER
+  /* ----------------------------------------------------------- */
+
+    jQuery('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+  /* ----------------------------------------------------------- */
+  /*  8. RELATED ITEM SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */    
+
+    jQuery('#mu-related-item-slide').slick({
+      dots: false,
+      arrows: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  9. MIXIT FILTER (FOR GALLERY) 
+  /* ----------------------------------------------------------- */  
+
+    jQuery(function(){
+      jQuery('#mixit-container').mixItUp();
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  10. FANCYBOX (FOR PORTFOLIO POPUP VIEW) 
+  /* ----------------------------------------------------------- */ 
+      
+    jQuery(document).ready(function() {
+      jQuery(".fancybox").fancybox();
+    });
+  
+  /* ----------------------------------------------------------- */
+  /*  11. HOVER DROPDOWN MENU
+  /* ----------------------------------------------------------- */ 
+  
+  // for hover dropdown menu
+    jQuery('ul.nav li.dropdown').hover(function() {
+      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+
+    
+  /* ----------------------------------------------------------- */
+  /*  12. SCROLL TOP BUTTON
+  /* ----------------------------------------------------------- */
+
+  //Check to see if the window is top if not then display button
+
+    jQuery(window).scroll(function(){
+      if (jQuery(this).scrollTop() > 300) {
+        jQuery('.scrollToTop').addClass('transformed-top-50');
+      } else {
+        jQuery('.scrollToTop').removeClass('transformed-top-50');
+      }
+    });
+     
+    //Click event to scroll to top
+
+    jQuery('.scrollToTop').click(function(){
+      jQuery('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+
+    var marquee = $('.marquee-body');
+    marquee.marquee({
+        //speed in milliseconds of the marquee
+        duration: 10000,
+        //gap in pixels between the tickers
+        gap: 50,
+        //time in milliseconds before the marquee will start animating
+        delayBeforeStart: 0,
+        //'left' or 'right'
+        direction: 'left',
+        //true or false - should the marquee be duplicated to show an effect of continues flow
+        // duplicated:true,
+        allowCss3Support:true,
+        pauseOnHover:true
+    });
+
+    var $topHeader = $('.top-header');
+    var $homeSlider = $('#mu-slider');
+    $topHeaderHeight = $topHeader.height();
+    $topHeader.waypoint(function (direction) {
+        if(direction == 'down'){
+            console.log('scrolling down');
+            $topHeader.addClass('sticky');
+            $homeSlider.css('margin-top',$topHeaderHeight+'px');
+        }
+    });
+
+    $topHeader.waypoint(function (direction) {
+        if(direction == 'up'){
+            console.log('scrolling up');
+            $topHeader.removeClass('sticky');
+            $homeSlider.css('margin-top','0');
+        }
+    });
+
+
+
+
+
+
+});
+
+$(document).ready(function () {
+    new WOW().init();
+});
+
